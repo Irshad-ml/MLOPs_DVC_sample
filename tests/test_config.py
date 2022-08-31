43c3fb626433276d6795bf78db1570d1cc580a8a
Md Irshad <m3irshad3@gmail.com>
2022-09-01 01:53:32 +0530

    updated Pytest example and also did linting

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,14 @@
-def test_generic():
-    a=10
-    b=10
-    assert a==b 
+class NotInRange(Exception):
+    def __init__(self,message = "value not in range"):
+        self.message=message
+        super().__init__(self.message)
+        
+        
+def test_range():
+    a=12
+    if a not in range(10,20):
+        raise NotInRange
+    else:
+        assert True
+        
+
