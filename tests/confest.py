fd4cec4ae6489bd8fb5581cf09f82d0a4acd56cd
Md Irshad <m3irshad3@gmail.com>
2022-09-03 23:37:20 +0530

    confest file update and dvc updated

--- tests/confest.py
+++ tests/confest.py
@@ -10,8 +10,8 @@ def config(config_path="params.yaml"):
         config = yaml.safe_load(yaml_file)
     return config
 
-# @pytest.fixture
-# def schema_in(schema_path="schema_in.json"):
-#     with open(schema_path) as json_file:
-#         schema = json.load(json_file)
-#     return schema
+@pytest.fixture
+def schema_in(schema_path="schema_in.json"):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
